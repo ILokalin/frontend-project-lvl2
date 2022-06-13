a084f4ec5a45c5a6993572a222791e8b1c207ace
Ilya Lokalin <ilyalokalin@gmail.com>
2022-06-14 00:25:55 +0500

    feat: add arguments #STEP-2

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import { Command } from 'commander';
 const app = () => {
   const program = new Command;
   program
+    .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>')
     .parse();
-
 };
 
 export default app;
