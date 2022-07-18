7e8c4604857e694f5747887b343b608dd5219100
Ilya Lokalin <ilyalokalin@gmail.com>
2022-07-19 04:35:03 +0500

    feat: update names #STEP-6

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,23 +7,23 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getActualPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFixture = (filename) => readFileSync(getActualPath(filename), 'utf-8').trim();
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-const diffJson = readFixture('diff_json.txt');
-const diffYml = readFixture('diff_yml.txt');
+const diffJson = readFile('diff_json.txt');
+const diffYml = readFile('diff_yml.txt');
 
 describe('app test', () => {
   it('genDiff json', () => {
-    const filepath1 = getActualPath('file_1.json');
-    const filepath2 = getActualPath('file_2.json');
+    const filepath1 = getFixturePath('file_1.json');
+    const filepath2 = getFixturePath('file_2.json');
 
     expect(genDiff(filepath1, filepath2)).toBe(diffJson);
   });
 
   it('genDiff yml', () => {
-    const filepath1 = getActualPath('file_1.yml');
-    const filepath2 = getActualPath('file_2.yml');
+    const filepath1 = getFixturePath('file_1.yml');
+    const filepath2 = getFixturePath('file_2.yml');
 
     expect(genDiff(filepath1, filepath2)).toBe(diffYml);
   });
